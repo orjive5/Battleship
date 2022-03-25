3ed983797d2ee01b3ccd0ee9ef686d5c9de4a7ed
German Dojcinovic <germandoicinovic@gmail.com>
2022-03-25 19:25:49 +0100

    Improved functionality - the game is playable.

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ const Ship = (position, length) => {
         return hitAt;
     }
     const isSunk = () => {
-        if (hitAt.length >= this.length){
-            return 'Sunk'
+        if (hitAt.length >= length){
+            return true;
         } else {
-            return 'Not sunk'
+            return false;
         }
     }
     return {position, length, shipCells, coordinates, hitAt, hit, isSunk}
@@ -32,54 +32,63 @@ const Gameboard = () => {
     const shipArray = [];
         const createShip = (position, length) => {
             shipArray.push(Ship(position, length));
-            if (position < 100){
-                for (let i = position-1; i < (position-1)+length; i++){
-                    if (gridCell[i].classList.contains('player-grid-cell-battleship')){
-                        console.log('try different length and position');
-                        shipArray.pop();
-                            while (i > position - 1){
-                                gridCell[i-1].classList.remove('player-grid-cell-battleship');
-                            i--;
+                if (position < 100){
+                    for (let i = position-1; i < (position-1)+length; i++){
+                        if (gridCell[i].classList.contains('player-grid-cell-battleship')){
+                            console.log('try different length and position');
+                            shipArray.pop();
+                                while (i > position - 1){
+                                    gridCell[i-1].classList.remove('player-grid-cell-battleship');
+                                i--;
+                            }
+                            break;
+                        } else {
+                            gridCell[i].classList.add('player-grid-cell-battleship');
                         }
-                        break;
-                    } else {
-                        gridCell[i].classList.add('player-grid-cell-battleship');
+                    }
+                } else {
+                    for (let i = position-1; i < (position-1)+length; i++){
+                        if (gridCell[i].classList.contains('computer-grid-cell-battleship')){
+                            console.log('try different length and position');
+                            shipArray.pop();
+                                while (i > position - 1){
+                                    gridCell[i-1].classList.remove('computer-grid-cell-battleship');
+                                i--;
+                            }
+                            break;
+                        } else {
+                            gridCell[i].classList.add('computer-grid-cell-battleship');
+                        }
                     }
                 }
-            } else {
-                for (let i = position-1; i < (position-1)+length; i++){
-                    if (gridCell[i].classList.contains('computer-grid-cell-battleship')){
-                        console.log('try different length and position');
-                        shipArray.pop();
-                            while (i > position - 1){
-                                gridCell[i-1].classList.remove('computer-grid-cell-battleship');
-                            i--;
-                        }
-                        break;
-                    } else {
-                        gridCell[i].classList.add('computer-grid-cell-battleship');
-                    }
-                }
-            }
-        return shipArray;
-    }
+            return shipArray;
+        }
     const recordMissedShots = [];
     const receiveAttack = (attackPosition) => {
-        //TODO
-        console.log('TO DO');
-        // if (attackX === this.newShip.x && attackY === this.newShip.y){
-        //     return this.newShip.hit(1);
-        // } else {
-        //     let missedShot = [attackX, attackY];
-        //     return recordMissedShots.push(missedShot);
-        // }
+        for (let j = 0; j < shipArray.length; j++){
+            if (shipArray[j].shipCells.includes(attackPosition)){
+                return shipArray[j].hit(attackPosition);
+            }
+        }
+        return recordMissedShots.push(attackPosition);
     }
-    return {createShip, shipArray, receiveAttack, recordMissedShots}
+    const allSunk = () => {
+        let sunkArray = [];
+        sunkArray = shipArray.map(el => el.isSunk());
+        if (sunkArray.includes(false)){
+            return 'Not sunk'
+        } else {
+            return 'All sunk. Game over'
+        }
+    }
+    return {createShip, shipArray, receiveAttack, recordMissedShots, allSunk}
 }
 
 // PLAYERS FACTORY
 
 const Player = () => {
+    const hitComputerCells = [];
+    const hitPlayerCells = [];
     const playerGameboardShipCells = [];
     playerGameboard.shipArray.forEach(element => element.shipCells.forEach(el => playerGameboardShipCells.push(el)));
     const computerGameboardShipCells = [];
@@ -87,11 +96,14 @@ const Player = () => {
     const playerPlays = () => {
         for (let i = 100; i < gridCell.length; i++){
                 gridCell[i].addEventListener('click', () => {
-                    if (gridCell[i].classList.contains('grid-cell-hit')){
+                    computerGameboard.receiveAttack(i);
+                    console.log(computerGameboard.allSunk());
+                    if (hitComputerCells.includes(i)){
                         console.log('Try again!');
                     }
-                    else if (gridCell[i].classList.contains('computer-grid-cell-battleship')){
+                    else if (computerGameboardShipCells.includes(i)){
                         gridCell[i].classList.add('grid-cell-hit');
+                        hitComputerCells.push(i);
                     } else {
                         gridCell[i].classList.add('grid-cell-attacked');
                     }
@@ -101,16 +113,19 @@ const Player = () => {
     }
     const computerPlays = () => {
         const computerAttack = Math.floor(Math.random() * 100);
-        if (gridCell[computerAttack].classList.contains('grid-cell-hit')){
+        playerGameboard.receiveAttack(computerAttack);
+        console.log(playerGameboard.allSunk());
+        if (hitPlayerCells.includes(computerAttack)){
             computerPlays();
         }
-        else if (gridCell[computerAttack].classList.contains('player-grid-cell-battleship')){
+        else if (playerGameboardShipCells.includes(computerAttack)){
             gridCell[computerAttack].classList.add('grid-cell-hit');
+            hitPlayerCells.push(computerAttack);
         } else {
             gridCell[computerAttack].classList.add('grid-cell-attacked');
         }
     }
-    return {playerGameboardShipCells, computerGameboardShipCells, playerPlays, computerPlays}
+    return {hitComputerCells, hitPlayerCells, playerGameboardShipCells, computerGameboardShipCells, playerPlays, computerPlays}
 }
 
 //MAIN GAME LOOP
