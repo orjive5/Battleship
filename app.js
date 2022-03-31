aed30fabc34a5da98cc558a4b7b2c5217af276f7
German Dojcinovic <germandoicinovic@gmail.com>
2022-03-31 23:29:13 +0200

    Fix game loop JS and UI. NEXT: Improve UI, complete game loop, fix Populating Computer Gameboard function (bug), make AI smarter, implement drag and drop to predetermine shipp coordinates

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ const Ship = (position, length) => {
         }
     }
 
-    return {position, length, shipCells, hitAt, hit, isSunk}
+    return {
+        position,
+        length,
+        shipCells,
+        hitAt,
+        hit,
+        isSunk
+    }
 }
 
 // GAMEBOARD FACTORY
@@ -126,12 +133,32 @@ const Gameboard = () => {
                 gridCell[i].classList.remove('computer-grid-cell-battleship');
             }
 
+            document.querySelector('.player-gameboard').style.display = 'none';
+            document.querySelector('.ai-gameboard').style.display = 'none';
+            document.querySelector('.player-gameboard-name').style.display = 'none';
+            appendNewElement(
+                'button', 
+                 document.querySelector('.gameboards'), 
+                 'Create New Game', 
+                 'create-new-game-button'
+                );
+            document.querySelector('.gameboards').style.justifyContent = 'center';
+            // userInformation.style.display = 'flex';
+
             return console.log('GAME OVER');
         }
 
     }
 
-    return {createShip, shipArray, allShipCells, receiveAttack, recordMissedShots, allSunk, gameOver}
+    return {
+        createShip,
+        shipArray,
+        allShipCells,
+        receiveAttack,
+        recordMissedShots,
+        allSunk,
+        gameOver
+    }
 }
 
 // PLAYERS FACTORY
@@ -143,7 +170,7 @@ const Player = () => {
 
     const playerPlays = () => {
 
-        for (let i = 100; i < gridCell.length; i++){
+            for (let i = 100; i < gridCell.length; i++){
                 gridCell[i].addEventListener('click', () => {
                     if (hitComputerCells.includes(i)){
 
@@ -160,7 +187,6 @@ const Player = () => {
                         computerGameboard.receiveAttack(i);
                         computerPlays();
                         computerGameboard.gameOver();
-                        playerGameboard.gameOver();
 
                     } else {
 
@@ -171,7 +197,7 @@ const Player = () => {
 
                     }
                 });
-        }
+            }
 
     }
 
@@ -203,7 +229,12 @@ const Player = () => {
 
     }
 
-    return {hitComputerCells, hitPlayerCells, playerPlays, computerPlays}
+    return {
+        hitComputerCells,
+        hitPlayerCells,
+        playerPlays,
+        computerPlays
+    }
 }
 
 //MAIN GAME LOOP
@@ -213,35 +244,112 @@ const gridCell = document.querySelectorAll('.grid-cell');
 
 //POPULATE GAMEBOARDS
 
-//Create Player gameboard
+const gameboards = document.querySelector('.gameboards');
+
+//Create gameboards
 const playerGameboard = Gameboard();
-
-//Populate Player gameboard
-playerGameboard.createShip(12,5);
-
-playerGameboard.createShip(32,4);
-
-playerGameboard.createShip(52,3);
-
-playerGameboard.createShip(72,2);
-
-playerGameboard.createShip(92,1);
-
-//Create Computer gameboard
 const computerGameboard = Gameboard();
 
+//POPULATE PLAYER GAMEBOARD
+
+//Append new element to the DOM function
+function appendNewElement(element, parent, text, id) {
+    const el = document.createElement(element);
+    if (text) {
+      el.innerHTML = text;
+    }
+    if (id) {
+      el.id = id;
+    }
+    parent.appendChild(el);
+    return el;
+}
+
+//Append elements
+appendNewElement('div', gameboards, undefined, 'user-information');
+const userInformation = document.querySelector('#user-information');
+userInformation.style.display = 'flex';
+userInformation.style.flexDirection = 'column';
+
+appendNewElement('label', userInformation, 'Choose ship position:', 'choose-ship-position');
+const chooseShipPosition = document.querySelector('#choose-ship-position');
+chooseShipPosition.setAttribute('for', 'ship-position-number');
+
+appendNewElement('input', userInformation, undefined, 'ship-position-number');
+const shipPositionNumber = document.querySelector('#ship-position-number');
+shipPositionNumber.setAttribute('placeholder', 'Enter number 0-99');
+shipPositionNumber.setAttribute('type', 'text');
+
+appendNewElement('label', userInformation, 'Choose ship length:', 'choose-ship-length');
+const chooseShipLength = document.querySelector('#choose-ship-length');
+chooseShipLength.setAttribute('for', 'ship-length-number');
+
+appendNewElement('input', userInformation, undefined, 'ship-length-number');
+const shipLengthNumber = document.querySelector('#ship-length-number');
+shipLengthNumber.setAttribute('placeholder', 'Enter number 1-5');
+shipLengthNumber.setAttribute('type', 'text');
+
+appendNewElement('button', userInformation, 'Create ship', 'create-ship-button');
+const createShipButton = document.querySelector('#create-ship-button');
+
+appendNewElement('label', userInformation, 'Enter your name:', 'label-for-username');
+const labelForUsername = document.querySelector('#label-for-username');
+labelForUsername.setAttribute('for', 'get-username');
+
+appendNewElement('input', userInformation, undefined, 'get-username');
+const getUsername = document.querySelector('#get-username');
+getUsername.setAttribute('placeholder', 'Your name');
+getUsername.setAttribute('type', 'text');
+
+appendNewElement('button', userInformation, 'Start game', 'start-button');
+const startButton = document.querySelector('#start-button');
+
+//Create player's fleet
+createShipButton.addEventListener('click', () => {
+    if (Number(shipPositionNumber.value) < 0 || Number(shipPositionNumber.value) > 99 || shipPositionNumber.value === ''){
+        return console.log('try again with correct value');
+    } else if (Number(shipLengthNumber.value) < 1 || Number(shipLengthNumber.value) > 5 || shipLengthNumber.value === ''){
+        return console.log('try again with correct value');
+    } else if (playerGameboard.allShipCells.length >= 15){
+        return console.log(`you've already created all available ships`);
+    } else {
+        return playerGameboard.createShip(Number(shipPositionNumber.value), Number(shipLengthNumber.value));
+    }
+});
+
+//Name player's gameboard
+getUsername.addEventListener('input', () => {
+    document.querySelector('.player-gameboard-name').innerHTML = getUsername.value;
+});
+
 //Populate Computer gameboard
-computerGameboard.createShip(112,5);
+function populateComputerGameboard() {
+    if (computerGameboard.shipArray.length < 5){
+        for (let i = 1; i < 6; i++){
+            computerGameboard.createShip(Math.floor(Math.random() * 100 + 100), i);
+        }
+        let createdShipLengths = [];
+        computerGameboard.shipArray.forEach(element => createdShipLengths.push(element.length));
+        let requiredLengths = [1,2,3,4,5];
+        let differenceLengths = requiredLengths.filter(x => !createdShipLengths.includes(x));
+        while (differenceLengths.length > 0){
+            for (let j = 0; j < differenceLengths.length; j++){
+                computerGameboard.createShip(Math.floor(Math.random() * 100 + 100), differenceLengths[j]);
+            };
+            computerGameboard.shipArray.forEach(element => createdShipLengths.push(element.length));
+            differenceLengths = requiredLengths.filter(x => !createdShipLengths.includes(x));
+        }
+    }
+}
+//START THE GAME BY CLICKING THE START BUTTON
 
-computerGameboard.createShip(132,4);
-
-computerGameboard.createShip(152,3);
-
-computerGameboard.createShip(172,2);
-
-computerGameboard.createShip(192,1);
-
-//START THE GAME BY LETTING THE USER CLICK ON AN COMPUTER GAMEBOARD
+startButton.addEventListener('click', () => {
+    if (playerGameboard.shipArray.length === 5){
+        userInformation.style.display = 'none';
+        document.querySelector('.ai-gameboard').style.display = 'initial';
+        populateComputerGameboard();
+    }
+})
 
 const players = Player();
 
